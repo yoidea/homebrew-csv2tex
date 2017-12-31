dd7827ae51b596d4fd3d7027610fb3bb81582e34
Yoidea <yoidea.cc@gmail.com>
2017-12-31 16:21:11 +0900

    Add a file

---
+++ csv2tex.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Chikubeam < Formula
+  homepage "https://github.com/yoidea/csv2tex"
+  url "https://github.com/yoidea/csv2tex/archive/v0.1.tar.gz"
+  sha256 "786618aa16451c7df18c3f97d3b79068f4db258f7f8b95be300cee651cc80076"
+  head "https://github.com/masawada/csv2tex.git"
+  version "0.1"
+
+  def install
+    system "make"
+    bin.install "csv2tex"
+  end
+end
