e91891353fa7311fe41e9336ca91e787a729e673
Yoidea <yoidea.cc@gmail.com>
2017-12-31 16:23:02 +0900

    Oops

--- csv2tex.rb
+++ csv2tex.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Chikubeam < Formula
+class Csv2tex < Formula
   homepage "https://github.com/yoidea/csv2tex"
   url "https://github.com/yoidea/csv2tex/archive/v0.1.tar.gz"
   sha256 "786618aa16451c7df18c3f97d3b79068f4db258f7f8b95be300cee651cc80076"
